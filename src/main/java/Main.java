770276b568cf96b1fd0c390a7372ae1ccd637ad6
Bata Kole <markosulaja@gmail.com>
2020-11-13 17:08:23 +0100

    radi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,30 @@ import java.util.ArrayList;
 
 public class Main {
 
-
-
-
     public static void main(String[] args) {
+
+        // First, ensure there are 2 args
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Exactly 2 parameters required !");
+        }
+        String jsonPath = args[0];
+        int partsNum = Integer.parseInt(args[1]);
 
 
         SplitDf splitDf = new SplitDf();
 
-        String jsonPath = "./data/test.jsonl";
 
-        ArrayList<Dataset<Row>> dfList = splitDf.getDfList(jsonPath);
+        ArrayList<Dataset<Row>> dfList = splitDf.getDfList(jsonPath, partsNum);
 
         System.out.println("List length: " + dfList.size());
 
+        int i = 1;
         for (Dataset<Row> df: dfList) {
             df.show();
+
+            // Save parts to output dir as json
+            df.write().json("./data/output/out" + i + ".json");
+            i++;
         }
 
     }
